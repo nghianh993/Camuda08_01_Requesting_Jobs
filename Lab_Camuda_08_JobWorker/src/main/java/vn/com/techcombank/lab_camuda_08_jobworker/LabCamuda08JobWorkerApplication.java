e06bdf2c156ebae698238b3115acd68c3c59e289
nghianh993 <nghianh993@gmail.com>
2025-02-17 11:46:34 +0700

    first commit

---
+++ Lab_Camuda_08_JobWorker/src/main/java/vn/com/techcombank/lab_camuda_08_jobworker/LabCamuda08JobWorkerApplication.java
@@ -0,0 +1,13 @@
+package vn.com.techcombank.lab_camuda_08_jobworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabCamuda08JobWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabCamuda08JobWorkerApplication.class, args);
+    }
+
+}
---
+++ Lab_Camuda_08_JobWorker/src/test/java/vn/com/techcombank/lab_camuda_08_jobworker/LabCamuda08JobWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.techcombank.lab_camuda_08_jobworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabCamuda08JobWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
